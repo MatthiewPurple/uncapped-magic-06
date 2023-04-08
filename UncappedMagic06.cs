a0481b2474757d4050cb2bd5e7f80c04020c22b6
Matthiew Purple <matthiewpurple@hotmail.com>
2023-04-08 15:46:15 +0200

    Add project files.

---
+++ UncappedMagic06.cs
@@ -0,0 +1,21 @@
+﻿using MelonLoader;
+using Il2Cpp;
+using uncapped_magic_06;
+
+[assembly: MelonInfo(typeof(UncappedMagic06), "Uncapped magic (ver. 0.6)", "1.0.0", "Matthiew Purple")]
+[assembly: MelonGame("アトラス", "smt3hd")]
+
+namespace uncapped_magic_06;
+public class UncappedMagic06 : MelonMod
+{
+    // When booting up the game
+    public override void OnInitializeMelon()
+    {
+        // For each skill in the game
+        for (int i = 0; i < datNormalSkill.tbl.Length; i++)
+        {
+            // If the skill is a magic skill, then uncap its limit
+            if (datNormalSkill.tbl[i].magiclimit != 0) datNormalSkill.tbl[i].magiclimit = short.MaxValue;
+        }
+    }
+}
